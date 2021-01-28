0d6a690c93507224197c5e91391882a8cd1e752b
stevenxu9494 <stevenxu9494@gmail.com>
2021-01-28 17:04:11 +1100

    first commit

---
+++ helloTS.js
@@ -0,0 +1,7 @@
+var a;
+a = 10;
+// js 中函数不考虑参数类型、个数
+function summ(a, b) {
+    return a + b;
+}
+console.log(summ(112, "123"));
---
+++ helloTS.ts
@@ -0,0 +1,9 @@
+var a:number;
+a = 10;
+
+// js 中函数不考虑参数类型、个数
+function summ(a:number, b:number) {
+  return a + b;
+}
+
+console.log(summ(112, "123"));
